fad974dcb97329afa55ce31042dbe233746edcfb
Afsana Faria <afsanafariya@gmail.com>
2021-10-29 21:59:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1isfb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("getSetGo");
+        const serviceCollection = database.collection("services");
+        const userCollection = database.collection("users");
+
+        //post api
+        app.post('/services', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService)
+            console.log('got new service', req.body);
+            console.log('added a new service', result);
+            res.json(result)
+        })
+        //get services
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        //get api by one
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            console.log("load user with id:", id)
+            res.send(service);
+        })
+        //delete api
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.deleteOne(query);
+            console.log("Delete the service with id:", id)
+            res.send(result);
+        })
+
+        //get users
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+        //post user
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await userCollection.insertOne(newUser)
+            console.log('got new user', req.body);
+            console.log('added a new user', result);
+            res.json(result)
+        })
+        //get a single user
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await userCollection.findOne(query);
+            console.log("load user with id:", id)
+            res.send(user);
+        })
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("Running my Delivery server")
+})
+
+app.listen(port, () => {
+    console.log("Running server on the port", port);
+})
